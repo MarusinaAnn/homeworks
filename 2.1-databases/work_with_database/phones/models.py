f4e46fa294313661e79a9830a6b53958031085f7
Ann Marusina <an.marusina2010@yandex.ru>
2023-02-25 20:31:53 +0300

    2.1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,20 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f"{self.id};" \
+               f" {self.name};" \
+               f" {self.price};" \
+               f" {self.image};" \
+               f" {self.release_date};" \
+               f" {self.lte_exists};" \
+               f" {self.slug}"
+    
