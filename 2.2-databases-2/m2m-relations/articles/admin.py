032a6f37027e4438150e040463d9ccc738552065
Ann Marusina <an.marusina2010@yandex.ru>
2023-02-26 21:29:47 +0300

    m2m

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet, ValidationError
 
-from .models import Article
+from .models import Article, ArticleScope, Scope
 
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        if len(self.forms) == 0:
+            raise ValidationError('Тематики не определены')
+
+        counter = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                counter += 1
+            if counter > 1:
+                raise ValidationError('Основная тема может быть только одна!')
+        return super().clean()  
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    formset = ArticleScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopeInline]
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(ArticleScope)
+class ArticleScopeAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,18 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Scope(models.Model):
+    tag = models.CharField(max_length=55, verbose_name='Название')
+    
+    class Meta:
+        verbose_name = 'Тематика'
+        verbose_name_plural = 'Тематики'
+
+    def __str__(self):
+        return self.tag
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,related_name='scopes')
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False)
